c4f7c8a7a8e19cc7eeb7239d1593c490c7e83b84
dibaya07 <dibyajyotibaruah987@gmail.com>
2026-07-29 12:57:14 +0530

    fix error

--- app/(client)/success/page.tsx
+++ app/(client)/success/page.tsx
@@ -1,37 +1,37 @@
 
-import { redirect } from 'next/navigation'
-import { stripe } from '@/lib/stripe'
-import { successSearchParamsProp } from '../types/product.types'
+// import { redirect } from 'next/navigation'
+// import { stripe } from '@/lib/stripe'
+// import { successSearchParamsProp } from '../types/product.types'
 
-export default async function page({ searchParams } : successSearchParamsProp ) {
-  const { session_id } = await searchParams
+// export default async function page({ searchParams } : successSearchParamsProp ) {
+//   const { session_id } = await searchParams
 
-  if (!session_id)
-    throw new Error('Please provide a valid session_id (`cs_test_...`)')
+//   if (!session_id)
+//     throw new Error('Please provide a valid session_id (`cs_test_...`)')
  
-  // console.log(await stripe.checkout.sessions)
-  const {
-    status,
-    customer_details //: { email: customerEmail}
-  } = await stripe.checkout.sessions.retrieve(session_id, {
-    expand: ['line_items', 'payment_intent']
-  })
+//   // console.log(await stripe.checkout.sessions)
+//   const {
+//     status,
+//     customer_details //: { email: customerEmail}
+//   } = await stripe.checkout.sessions.retrieve(session_id, {
+//     expand: ['line_items', 'payment_intent']
+//   })
 
-  const customerEmail = customer_details?.email
+//   const customerEmail = customer_details?.email
 
-  if (status === 'open') {
-    return redirect('/')
-  }
+//   if (status === 'open') {
+//     return redirect('/')
+//   }
 
-  if (status === 'complete') {
-    return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to{' '}
-          {customerEmail}. If you have any questions, please email{' '}
-        </p>
-        <a href="mailto:orders@example.com">orders@example.com</a>.
-      </section>
-    )
-  }
-}
+//   if (status === 'complete') {
+//     return (
+//       <section id="success">
+//         <p>
+//           We appreciate your business! A confirmation email will be sent to{' '}
+//           {customerEmail}. If you have any questions, please email{' '}
+//         </p>
+//         <a href="mailto:orders@example.com">orders@example.com</a>.
+//       </section>
+//     )
+//   }
+// }
