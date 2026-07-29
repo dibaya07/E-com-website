4c9a4f0e2f9668c55d94f22af04052f948c84d5d
dibaya07 <dibyajyotibaruah987@gmail.com>
2026-07-29 13:04:20 +0530

    fix error

--- app/(client)/checkOut/page.tsx
+++ app/(client)/checkOut/page.tsx
@@ -1,9 +1,9 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function page() {
-//   return (
-//     <div>
-//       Work in progress
-//     </div>
-//   )
-// }
+export default function page() {
+  return (
+    <div>
+      Work in progress
+    </div>
+  )
+}
